1e78e229ae1d6913f9a035f229828cff23e1688c
Aleksi Asikainen <aleksi.asikainen@gmail.com>
2022-09-02 12:33:42 -0700

    feat: add staging configuration

---
+++ config.stg.ts
@@ -0,0 +1,10 @@
+export const API_V2_URL = 'https://api-stg.arena.im/v2';
+export const CACHED_API = 'https://cached-api-stg.arena.im/v1';
+export const DEFAULT_AUTH_TOKEN =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NGQ5OGJiNmY3MDIyOGU4MWI4Njc5YmUiLCJyb2xlcyI6WyJVU0VSIl0sImV4cCI6MzM2OTQxODM2OSwiaWF0IjoxNDc3MjU4MzY5fQ.dNpdrs3ehrGAhnPFIlWMrQFR4mCFKZl_Lvpxk1Ddp4o';
+export const FIREBASE_APIKEY = 'AIzaSyBYdUhOc3KUsoX4ACgGd4LtJPKpNCsAgHs';
+export const FIREBASE_AUTHDOMAIN = 'arena-im-stg.firebaseapp.com';
+export const FIREBASE_PROJECT_ID = 'arena-im-stg';
+export const GRAPHQL_ENDPOINT = 'https://je65tljztzaeho3krdvfdyzooi.appsync-api.us-west-2.amazonaws.com/graphql';
+export const ARENA_REALTIME_URL = 'https://realtime-stg.arena.im';
+export const ARENA_URL = 'https://go-stg.arena.im';
