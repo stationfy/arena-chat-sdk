4ab1043f3634fea9167982c87485571993e68c6a
Cesar  Alvarenga <cesarwbr@gmail.com>
2023-05-09 18:29:26 -0700

    fix: add eu package

--- packages/chat-sdk/jest.config.js
+++ packages/chat-sdk/jest.config.js
@@ -1,20 +1,28 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
+import utils from 'ts-jest/utils/index.js';
+import tsconfig from './tsconfig.json' assert { type: "json" };
 
-module.exports = {
-  collectCoverage: true,
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
+export const collectCoverage = true;
+export const transform = {
+  '^.+\\.ts$': 'ts-jest',
+};
+export const moduleFileExtensions = ['js', 'ts'];
+export const testEnvironment = 'jsdom';
+export const testMatch = ['**/*.test.ts'];
+export const globals = {
+  'ts-jest': {
+    tsconfig: './tsconfig.json',
+    diagnostics: false,
   },
-  moduleFileExtensions: ['js', 'ts'],
-  testEnvironment: 'jsdom',
-  testMatch: ['**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.json',
-      diagnostics: false,
-    },
-  },
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+};
+export const moduleNameMapper = utils.pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: '<rootDir>/' });
+
+export default {
+  collectCoverage,
+  transform,
+  moduleFileExtensions,
+  testEnvironment,
+  testMatch,
+  globals,
+  moduleNameMapper
 };
