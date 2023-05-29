9ac73be7dcdd1dd948ad8983ffb380ff6604f064
edgardleal <edgardleal@gmail.com>
2023-05-29 16:03:47 -0300

    chore: include tina setup
    Tina is a service to help edit and maintain documentation
    pages using markdown.

---
+++ sdk-doc/tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "20483caa-850e-4d58-8f84-f59052fa0e50", // Get this from tina.io
+  token: "4a8b2e1c1cc45060e7d102ee9f5cdd533db7eb4f", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
