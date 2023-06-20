38e132f5c5b32d14d64a67ae7c970de08397f125
brunocavaliere <bcavaliere93@gmail.com>
2023-06-20 13:44:12 -0300

    feat(EUF-300): new doc

--- sdk-doc/docusaurus.config.js
+++ sdk-doc/docusaurus.config.js
@@ -9,55 +9,18 @@ const config = {
   title: 'Arena Documentation',
   tagline: 'Choose the toll',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'warn', // throw
+  organizationName: 'facebook',
+  projectName: 'docusaurus',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
 
   presets: [
-    // [
-    //   'classic',
-    //   /** @type {import('@docusaurus/preset-classic').Options} */
-    //   ({
-    //     docs: {
-    //       sidebarPath: require.resolve('./sidebars.js'),
-    //       // Please change this to your repo.
-    //     },
-    //     theme: {
-    //       customCss: require.resolve('./src/css/custom.css'),
-    //     },
-    //   }),
-    // ],
-    // [
-    //   '@docusaurus/plugin-content-docs',
-    //   {
-    //     // id: 'product', // omitted => default instance
-    //     path: 'apiv1',
-    //     routeBasePath: 'apiv1',
-    //     version: '1.0.0',
-    //     // sidebarPath: require.resolve('./sidebarsProduct.js'),
-    //     // ... other options
-    //   },
-    // ],
-
     [
       "docusaurus-preset-openapi",
       /** @type {import('docusaurus-preset-openapi').Options} */
@@ -80,27 +43,13 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/arena-logo.svg',
       navbar: {
-        // title: 'SDK',
         logo: {
           alt: 'Arena Logo',
           src: 'img/arena-logo.svg',
         },
         items: [
-          // {
-          //   type: 'doc',
-          //   docId: 'chat-sdk',
-          //   position: 'left',
-          //   label: 'Chat SDK',
-          // },
-          // {
-          //   type: 'doc',
-          //   docId: 'api',
-          //   position: 'left',
-          //   label: 'API V3',
-          // },
           {
             href: 'https://github.com/stationfy/arena-chat-sdk',
             label: 'GitHub',
@@ -108,47 +57,6 @@ const config = {
           },
         ],
       },
-      // footer: {
-      //   style: 'dark',
-      //   links: [
-      //     {
-      //       title: 'Docs',
-      //       items: [
-      //         {
-      //           label: 'Tutorial',
-      //           to: '/docs/intro',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'Community',
-      //       items: [
-      //         {
-      //           label: 'Stack Overflow',
-      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //         },
-      //         {
-      //           label: 'Discord',
-      //           href: 'https://discordapp.com/invite/docusaurus',
-      //         },
-      //         {
-      //           label: 'Twitter',
-      //           href: 'https://twitter.com/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'More',
-      //       items: [
-      //         {
-      //           label: 'GitHub',
-      //           href: 'https://github.com/facebook/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
